3e64c2b1580ae2d98aa25c88c7d1686ea3d2cb32
jessymihaja <jessyranivoaritida@gmail.com>
2023-06-08 21:34:24 +0300

    huhucmmit

--- Framework2/src/etu2046/framework/Modelview.java
+++ Framework2/src/etu2046/framework/Modelview.java
@@ -4,12 +4,16 @@
  */
 package etu2046.framework;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author jessy
  */
 public class Modelview {
     String view;
+    HashMap <String ,Object> data;
 
     public String getView() {
         return view;
@@ -19,8 +23,20 @@ public class Modelview {
         this.view = view;
     }
 
-    public Modelview(String view) {
-        this.setView(view);
+    public HashMap<String,Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap <String,Object> data) {
+        this.data = data;
     }
     
+    public Modelview(String view) {
+        this.setView(view);
+        this.setData(new HashMap<>());
+    }
+
+    public void addItem(String key,Object item){
+        this.getData().put(key,item);
+    }
 }
--- Framework_test/src/java/test/Test2.java
+++ Framework_test/src/java/test/Test2.java
@@ -6,14 +6,66 @@ package test;
 
 import annotation.Annotation;
 import etu2046.framework.Modelview;
+import java.util.ArrayList;
 
 /**
  *
  * @author jessy
  */
 public class Test2 {
-     @Annotation(url="/get_all")
-    public Modelview get_all_test(){
-        return new Modelview("test_view.jsp");
+   String nom;
+   String prenom;
+
+    public String getNom() {
+        return nom;
     }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Test2(String nom, String prenom) {
+        this.setNom(nom);
+        this.setPrenom(prenom);
+    }
+    public Test2(){
+    }
+    
+   
+   
+   
+    @Annotation(url="/get_all")
+    public Modelview get_all_test(){
+        Modelview model=new Modelview("test_view.jsp");
+        model.addItem("liste", allemp());
+        return model;
+    }
+    
+    
+    ArrayList<Test2> allemp(){
+        ArrayList<Test2> list=new ArrayList<>();
+        list.add(new Test2("jean","bernard"));
+        list.add(new Test2("huhu","betrand"));
+        list.add(new Test2("haha","bert"));
+        list.add(new Test2("hehe","bertold"));
+        return list;
+    }
+    @Annotation(url="/save")
+    public Modelview save(){
+        Modelview model=new Modelview("test_view.jsp");
+        ArrayList<Test2> list=new ArrayList<>();
+        list.add(this);
+        model.addItem("liste", list);
+        return model;
+        
+    }
+    
 }
